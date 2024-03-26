83a1f5e9b14cfa8d7273aa7492851615c4382b5d
Reybuts <butalreymart1996@gmail.com>
2024-03-26 15:20:21 +0800

    first commit

---
+++ lib/dashboard.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacementNamed(context, '/');
+    } catch (e) {
+      print('Error signing out: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              FontAwesomeIcons.signOut,
+              size: 40.10,
+              color: Colors.red,
+            ),
+            onPressed: () => _signOut(context),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome to the Dashboard!',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 16.0),
+            const Text(
+              'Logged in as:',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              user != null ? user.email ?? '' : '',
+              style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
